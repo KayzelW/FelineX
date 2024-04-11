f1bd4655eeb09cb9377fc190e6ae3be9f86776d4
Petrino411 <petr.siv@rambler.ru>
2024-04-11 17:29:33 +0300

    TestController

---
+++ APIServer/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using APIServer.Database;
+using Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared.DB.Classes;
+using Shared.DB.Classes.User;
+using Shared.Extensions;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace APIServer.Controllers;
+
+[ApiController, Route("[controller]")]
+public class TestController : Controller
+{
+    private readonly AppDbContext _dbContext;
+
+    public TestController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetTest()
+    {
+        // var test = await _dbContext.Tests
+        //     .Include(x => x.Tasks)
+        //     .Include(x => x.Creator)
+        //     .FirstOrDefaultAsync();
+        var test = await _dbContext.Tests.Include(x => x.Creator).ToListAsync();
+        Console.WriteLine(test[0].Creator);
+        return Ok(test);
+    }
+}
---
+++ WebApp/test.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO `Users` (`Id`, `UserName`, `NormalizedUserName`, `PasswordHash`, `Access`) VALUES 
+(1, 'root', NULL, 'root', '');
+INSERT INTO `Tests` (`Id`, `CreatorId`) VALUES ('1', 1);
+
+
