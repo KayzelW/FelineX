d82b8f8438fa3cd5d7a73e0f0d1eff8ce3c7958a
Kayzel <kapitonovroman5@gmail.com>
2024-04-22 14:27:59 +0300

    fix Create_test page
    fix

--- Shared/DB/Classes/Test/Task/Task.cs
+++ Shared/DB/Classes/Test/Task/Task.cs
@@ -9,12 +9,18 @@ public sealed partial class Task : ITask
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
 
-    [MaxLength(1000)] public string? Question { get; set; }
+    [MaxLength(1000)] public string? Question { get; set; } = "";
     [ForeignKey(nameof(ThemeTask))] public List<ThemeTask>? Thematics { get; set; } = new List<ThemeTask>();
     public InteractionType InteractionType { get; set; } = InteractionType.LongStringTask;
     public List<VariableAnswer>? VariableAnswers { get; set; } = new List<VariableAnswer>();
     [ForeignKey(nameof(User.User))] public User.User? Creator { get; set; }
 
+    [NotMapped]
+    public int CountVariables
+    {
+        get => VariableAnswers!.Count;
+        set => FixCountVariables(value);
+    }
 
     #region Constructors
 
@@ -22,14 +28,16 @@ public sealed partial class Task : ITask
     {
     }
 
-    public Task(string? question, InteractionType interactionType) : this()
+    public Task(string? question, InteractionType interactionType) :
+        this()
     {
         Question = question ?? "Вопрос задания";
         InteractionType = interactionType;
     }
 
-    public Task(string? question, InteractionType interactionType, params string[] answers) : this(question,
-        interactionType)
+    public Task(string? question, InteractionType interactionType, params string[] answers) :
+        this(question,
+            interactionType)
     {
         foreach (var answer in answers)
         {
@@ -38,4 +46,33 @@ public sealed partial class Task : ITask
     }
 
     #endregion
+
+    private void FixCountVariables(int targetCount)
+    {
+        int countInTask = VariableAnswers!.Count;
+
+        if (targetCount == 0)
+        {
+            VariableAnswers!.Clear();
+            return;
+        }
+
+        if (countInTask > targetCount)
+        {
+            for (int i = countInTask; i > targetCount; i--)
+            {
+                VariableAnswers!.RemoveAt(i);
+            }
+            return;
+        }
+
+        if (countInTask < targetCount)
+        {
+            for (int i = countInTask; i < targetCount; i++)
+            {
+                VariableAnswers!.Add(new VariableAnswer());
+            }
+            return;
+        }
+    }
 }
