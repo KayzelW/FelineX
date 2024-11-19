cc0a7cc122929fdc24c216b61852189db315290f
Егорова Виктория <viktori_700@mail.ru>
2024-11-19 06:27:37 +0300

    fixed

--- DesktopMAUIApp/Pages/TestResult.xaml.cs
+++ DesktopMAUIApp/Pages/TestResult.xaml.cs
@@ -2,12 +2,29 @@ namespace DesktopMAUIApp.Pages;
 
 public partial class TestResult : ContentPage
 {
-    private readonly TestResultPageModel _viewModel;
-    public TestResult(TestResultPageModel model)
+
+    public double? TestAnswerScore;
+    public string CatScoreImage;
+
+    private bool _isLoading;
+    private bool _isLoaded;
+    public TestResult()
 	{
-		//InitializeComponent();
-        BindingContext = model;
-        
+        InitializeComponent();
+        TestAnswerScore = 70; //await _apiService.GetTestScore(Guid.Parse(answeredTestId));
+        if (TestAnswerScore.HasValue)
+        {
+            var catScore = (int)Math.Floor(TestAnswerScore.Value / 10);
+            CatScoreImage = $"cat{catScore}.png";
+            //CatScoreImage = $"DesktopMAUIApp/Resources/images/cats/{catScore}.png";
+        }
+        CatScoreImage = "cat5.png";
+
+        _isLoaded = false;
+        _isLoaded = true;
+
+
+
     }
 
 
