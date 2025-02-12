567f957b3db5105ebf43e738bb09e3ff6afe7fff
Егорова Виктория <viktori_700@mail.ru>
2025-02-12 14:11:38 +0300

    clean up

--- Aspire/Aspire.AppHost/Program.cs
+++ Aspire/Aspire.AppHost/Program.cs
@@ -1,14 +1,25 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+var cache = builder.AddRedis("cache")
+    .WithRedisCommander()
+    .WithDataVolume(isReadOnly: false)
+    .WithPersistence(TimeSpan.FromMinutes(5), 5);
 
-var apiService = builder.AddProject<Projects.Aspire_ApiService>("apiservice");
+var username = builder.AddParameter("DbUsername");
+var password = builder.AddParameter("DbUserPassword");
+var postrges = builder
+    .AddPostgres("postrges", userName: username, password: password)
+    .WithDataVolume(isReadOnly: false);
+var db = postrges
+    .AddDatabase("FelineX");
+
+var apiService = builder.AddProject<Projects.Aspire_ApiService>("apiservice")
+    .WithReference(db).WaitFor(db);
 
 builder.AddProject<Projects.Aspire_Web>("webfrontend")
+    // .WithReplicas(2)
     .WithExternalHttpEndpoints()
-    .WithReference(cache)
-    .WaitFor(cache)
-    .WithReference(apiService)
-    .WaitFor(apiService);
+    .WithReference(cache).WaitFor(cache)
+    .WithReference(apiService).WaitFor(apiService);
 
 builder.Build().Run();
